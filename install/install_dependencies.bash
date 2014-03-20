0d4f1fdb74abde56034c118e40b831c55ec5f28a
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-03-19 22:57:30 -0400

    rough GL integration

--- install/install_dependencies.bash
+++ install/install_dependencies.bash
@@ -3,3 +3,7 @@ sudo apt-get install build-essential xorg-dev libudev-dev libts-dev libgl1-mesa-
 sudo apt-get install libgtk2.0-0:i386 libnss3-1d:i386 libnspr4-0d:i386 lib32nss-mdns libxml2:i386 libxslt1.1:i386
 
 sudo apt-get install libglib2.0-dev
+
+sudo apt-get install freeglut3-dev freeglut-devel
+
+sudo apt-get install libglew libglew-dev glew-utils
