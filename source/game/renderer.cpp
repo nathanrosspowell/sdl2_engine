0111c3be367e1c783594dbb3afedf4ab783ddd65
Nathan Ross Powell <nathanrosspowell@gmail.com>
2014-03-23 18:34:58 -0400

    refactored shader loading

--- source/game/renderer.cpp
+++ source/game/renderer.cpp
@@ -1,46 +1,59 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // entity/base.cpp Authored by Nathan Ross Powell
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // This header.
 #include "renderer.h"
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // SDL.
 #include "SDL.h"
 #include "SDL_image.h"
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// STD library.
+#include <fstream>
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 namespace game
 {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//Starts up SDL, creates window, and initializes OpenGL
-//Graphics program
-bool Renderer::init()
+Renderer::Renderer( const commandLine::Base& cmdLine, SDL_Window* window )
+: m_cmdLine( cmdLine )
+, m_window( window )
+, m_surfaceLoul( nullptr )
+, m_surfaceCat( nullptr )
+, m_programId( 0 )
+, m_vertexPos( -1 )
+, m_vbo( 0 )
+, m_ibo( 0 )
 {
-    //Initialization flag
+
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+bool Renderer::initalise()
+{
+    stateStd( "" );
+    m_surfaceLoul = loadBmpToSurface( "../resources/image/test.bmp" );
+    m_surfaceCat = loadBmpToSurface( "../resources/image/cat.bmp" );
+        //Initialization flag
     bool success = true;
     //Create context
     m_context = SDL_GL_CreateContext( m_window );
     if( m_context == NULL )
     {
-        errorStd( "OpenGL context could not be created! SDL Error:" << SDL_GetError() );
+        errorStd( "OpenGL not created! SDL Error:" << SDL_GetError() );
         success = false;
     }
     else
     {
         //Initialize GLEW
         glewExperimental = GL_TRUE;
-        GLenum glewError = glewInit();
-        if( glewError != GLEW_OK )
+        GLenum error = glewInit();
+        if( error != GLEW_OK )
         {
-            errorStd( "Error initializing GLEW! Error:" << glewGetErrorString( glewError ) );
+            errorStd( "Initializing GLEW: " << glewGetErrorString( error ) );
         }
         //Use Vsync
         if( SDL_GL_SetSwapInterval( 1 ) < 0 )
         {
-            errorStd( "Warning: Unable to set VSync! SDL Error: " << SDL_GetError() );
+            errorStd( "Unable to set VSync! SDL Error: " << SDL_GetError() );
         }
         //Initialize OpenGL
         if( !initGL() )
@@ -51,150 +64,130 @@ bool Renderer::init()
     }
     return success;
 }
-
 bool Renderer::initGL()
 {
-    //Success flag
-    bool success = true;
     //Generate program
-    m_programIDbo = glCreateProgram();
+    m_programId = glCreateProgram();
     //Create vertex shader
-    GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
-    //Get vertex source
-    const GLchar* vertexShaderSource[] =
+    std::string vertexPath = "../source/shaders/vertex/basic.glvs";
+    GLuint vertexId = loadShaderFromFile( vertexPath, GL_VERTEX_SHADER );
+    if( vertexId == 0 )
     {
-        "#version 120\n"
-        "attribute vec2 LVertexPos2D;"
-        "void main()"
-        "{"
-            "gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 );"
-        "}"
-    };
-    //Set vertex source
-    glShaderSource( vertexShader, 1, vertexShaderSource, NULL );
-    //Compile vertex source
-    glCompileShader( vertexShader );
-    //Check vertex shader for errors
-    GLint vShaderCompiled = GL_FALSE;
-    glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &vShaderCompiled );
-    if( vShaderCompiled != GL_TRUE )
-    {
-        printf( "Unable to compile vertex shader %d!\n", vertexShader );
-        printShaderLog( vertexShader );
-        success = false;
+        return false;
     }
     else
     {
         //Attach vertex shader to program
-        glAttachShader( m_programIDbo, vertexShader );
-        //Create fragment shader
-        GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-        //Get fragment source
-        const GLchar* fragmentShaderSource[] =
-        {
-            "#version 120\n"
-            "void main()"
-            "{"
-                "gl_FragColor = vec4( 0.0, 1.0, 0.6, 1.0 );"
-            "}"
-        };
-        //Set fragment source
-        glShaderSource( fragmentShader, 1, fragmentShaderSource, NULL );
-        //Compile fragment source
-        glCompileShader( fragmentShader );
-        //Check fragment shader for errors
-        GLint fShaderCompiled = GL_FALSE;
-        glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &fShaderCompiled );
-        if( fShaderCompiled != GL_TRUE )
-        {
-            printf( "Unable to compile fragment shader %d!\n", fragmentShader );
-            printShaderLog( fragmentShader );
-            success = false;
-        }
-      else
-      {
-            //Attach fragment shader to program
-            glAttachShader( m_programIDbo, fragmentShader );
-            //Link program
-            glLinkProgram( m_programIDbo );
-            //Check for errors
-            GLint programSuccess = GL_TRUE;
-            glGetProgramiv( m_programIDbo, GL_LINK_STATUS, &programSuccess );
-            if( programSuccess != GL_TRUE )
-            {
-                printf( "Error linking program %d!\n", m_programIDbo );
-                printProgramLog( m_programIDbo );
-                success = false;
-            }
-            else
-            {
-                //Get vertex attribute location
-                m_vertexPos2DLocation = glGetAttribLocation( m_programIDbo, "LVertexPos2D" );
-                if( m_vertexPos2DLocation == -1 )
-                {
-                    printf( "LVertexPos2D is not a valid glsl program variable!\n" );
-                    success = false;
-                }
-                else
-                {
-                    //Initialize clear color
-                    glClearColor( 0.f, 0.f, 0.f, 1.f );
-                    //VBO data
-                    GLfloat vertexData[] =
-                    {
-                        -0.5f, -0.5f,
-                        0.5f, -0.5f,
-                        0.5f,  0.5f,
-                        -0.5f,  0.5f
-                    };
-                    //IBO data
-                    GLuint indexData[] = { 0, 1, 2, 3 };
-                    //Create VBO
-                    glGenBuffers( 1, &m_vbo );
-                    glBindBuffer( GL_ARRAY_BUFFER, m_vbo );
-                    glBufferData( GL_ARRAY_BUFFER, 2 * 4 * sizeof(GLfloat), vertexData, GL_STATIC_DRAW );
-                    //Create IBO
-                    glGenBuffers( 1, &m_ibo );
-                    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_ibo );
-                    glBufferData( GL_ELEMENT_ARRAY_BUFFER, 4 * sizeof(GLuint), indexData, GL_STATIC_DRAW );
-                }
-            }
-        }
+        glAttachShader( m_programId, vertexId );
     }
-    return success;
+    //Create vertex shader
+    std::string fragmentPath = "../source/shaders/fragment/basic.glfs";
+    GLuint fragmentId = loadShaderFromFile( fragmentPath, GL_FRAGMENT_SHADER );
+    if( fragmentId == 0 )
+    {
+        errorStd( "Can't compile fragment shader: " << fragmentId );
+        return false;
+    }
+    //Attach fragment shader to program
+    glAttachShader( m_programId, fragmentId );
+    //Link program
+    glLinkProgram( m_programId );
+    //Check for errors
+    GLint programSuccess = GL_TRUE;
+    glGetProgramiv( m_programId, GL_LINK_STATUS, &programSuccess );
+    if( programSuccess != GL_TRUE )
+    {
+        errorStd( "Error linking program: " << m_programId );
+        printProgramLog( m_programId );
+        return false;
+    }
+    //Get vertex attribute location
+    m_vertexPos = glGetAttribLocation( m_programId, "LVertexPos2D" );
+    if( m_vertexPos == -1 )
+    {
+        errorStd( "LVertexPos2D is not a valid glsl program variable." );
+        return false;
+    }
+    //Initialize clear color
+    glClearColor( 0.f, 0.f, 0.f, 1.f );
+    //VBO data
+    GLfloat vertexData[] =
+    {
+        -0.5f, -0.5f,
+        0.5f, -0.5f,
+        0.5f,  0.5f,
+        -0.5f,  0.2f
+    };
+    //IBO data
+    GLuint indexData[] = { 0, 1, 2, 3 };
+    //Create VBO
+    glGenBuffers( 1, &m_vbo );
+    glBindBuffer( GL_ARRAY_BUFFER, m_vbo );
+    glBufferData( GL_ARRAY_BUFFER
+      , 2 * 4 * sizeof(GLfloat)
+      , vertexData
+      , GL_STATIC_DRAW );
+    //Create IBO
+    glGenBuffers( 1, &m_ibo );
+    glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_ibo );
+    glBufferData( GL_ELEMENT_ARRAY_BUFFER
+      , 4 * sizeof(GLuint)
+      , indexData
+      , GL_STATIC_DRAW );
+    return true;
 }
 
+bool Renderer::shutDown()
+{
+    stateStd( "" );
+    SDL_FreeSurface( m_surfaceLoul );
+    SDL_FreeSurface( m_surfaceCat );
+    glDeleteProgram( m_programId );
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void Renderer::update()
+{
+    //Render quad
+    render();
+    //Update screen
+    SDL_GL_SwapWindow( m_window );
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void  Renderer::render()
 {
     //Clear color buffer
     glClear( GL_COLOR_BUFFER_BIT );
     //Bind program
-    glUseProgram( m_programIDbo );
+    glUseProgram( m_programId );
     //Enable vertex position
-    glEnableVertexAttribArray( m_vertexPos2DLocation );
+    glEnableVertexAttribArray( m_vertexPos );
     //Set vertex data
     glBindBuffer( GL_ARRAY_BUFFER, m_vbo );
-    glVertexAttribPointer( m_vertexPos2DLocation, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), NULL );
+    glVertexAttribPointer( m_vertexPos
+      , 2
+      , GL_FLOAT
+      , GL_FALSE
+      , 2 * sizeof(GLfloat)
+      , NULL );
     //Set index data and render
     glBindBuffer( GL_ELEMENT_ARRAY_BUFFER, m_ibo );
     glDrawElements( GL_TRIANGLE_FAN, 4, GL_UNSIGNED_INT, NULL );
     //Disable vertex position
-    glDisableVertexAttribArray( m_vertexPos2DLocation );
+    glDisableVertexAttribArray( m_vertexPos );
     //Unbind program
     glUseProgram( 0 );
 }
-
-void  Renderer::close()
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SDL_Surface* Renderer::loadBmpToSurface( const std::string& file )
 {
-    //Deallocate program
-    glDeleteProgram( m_programIDbo );
-    //Destroy window  
-    SDL_DestroyWindow( m_window );
-    m_window = NULL;
-    //Quit SDL subsystems
-    SDL_Quit();
+    SDL_Surface* surface = SDL_LoadBMP( file.c_str() );
+    if ( surface == nullptr )
+    {
+        errorStd( "Missing surface: " << file << " SDL:" << SDL_GetError() );
+    }
+    return surface;
 }
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 void  Renderer::printProgramLog( GLuint program )
 {
     //Make sure name is shader
@@ -212,18 +205,18 @@ void  Renderer::printProgramLog( GLuint program )
         if( infoLogLength > 0 )
         {
             //Print Log
-            printf( "%s\n", infoLog );
+            errorStd( infoLog );
         }
         //Deallocate string
         delete[] infoLog;
     }
     else
     {
-        printf( "Name %d is not a program\n", program );
+        errorStd( "Name " << program << " is not a program" );
     }
 }
-
-void  Renderer::printShaderLog( GLuint shader )
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void Renderer::printShaderLog( GLuint shader )
 {
     //Make sure name is shader
     if( glIsShader( shader ) )
@@ -240,108 +233,47 @@ void  Renderer::printShaderLog( GLuint shader )
         if( infoLogLength > 0 )
         {
             //Print Log
-            printf( "%s\n", infoLog );
+            errorStd( infoLog );
         }
         //Deallocate string
         delete[] infoLog;
     }
     else
     {
-        printf( "Name %d is not a shader\n", shader );
+        errorStd( "Name " << shader << " is not a shader" );
     }
 }
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Renderer::Renderer( const commandLine::Base& cmdLine, SDL_Window* window )
-: m_cmdLine( cmdLine )
-, m_window( window )
-, m_background( nullptr )
-, m_image( nullptr )
-, m_programIDbo( 0 )
-, m_vertexPos2DLocation( -1 )
-, m_vbo( 0 )
-, m_ibo( 0 )
+GLuint Renderer::loadShaderFromFile( std::string path, GLenum shaderType )
 {
-
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bool Renderer::initalise()
-{
-    stateStd( "" );
-    m_background = loadTexture( "../resources/image/test.bmp" );
-    m_image = loadTexture( "../resources/image/cat.bmp" );
-    init();
-}
-bool Renderer::shutDown()
-{
-    stateStd( "" );
-    SDL_DestroyTexture( m_background );
-    SDL_DestroyTexture( m_image );
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-void Renderer::update()
-{
-    //Render quad
-    render();
-    
-    //Update screen
-    SDL_GL_SwapWindow( m_window );
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bool Renderer::draw( SDL_Texture* texture
-                   , int x
-                   , int y
-                   , double rotation )
-{
-    SDL_Rect dst;
-    dst.x = x;
-    dst.y = y;
-    //SDL_QueryTexture( texture, NULL, NULL, &dst.w, &dst.h);
-    //SDL_RenderCopy( m_renderer, texture, NULL, &dst);
-    return true;
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bool Renderer::draw( SDL_Texture* texture
-                   , int x
-                   , int y
-                   , int x2
-                   , int y2
-                   , int w
-                   , int h
-                   , double rotation )
-{
-    return true;
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bool Renderer::draw( SDL_Texture* texture
-                   , int x
-                   , int y
-                   , SDL_Rect& rect
-                   , double rotation )
-{
-    return true;
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-bool Renderer::draw( int x
-                   , int y
-                   , int w
-                   , int h
-                   , SDL_Color& color
-                   , double rotation )
-{
-    return true;
-}
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-SDL_Texture* Renderer::loadTexture( const std::string &file )
-{
-    SDL_Texture* texture = nullptr;// = IMG_LoadTexture( m_renderer, file.c_str());
-    if ( texture == nullptr )
+    GLuint shaderId = 0;
+    std::ifstream sourceFile( path.c_str() );
+    if( sourceFile )
     {
-        errorStd( "Missing texture: " << file << " SDL:" << SDL_GetError() );
+        auto stream = std::istreambuf_iterator< char >( sourceFile );
+        auto iterator = std::istreambuf_iterator< char >();
+        std::string shaderString;
+        shaderString.assign( stream, iterator );
+        shaderId = glCreateShader( shaderType );
+        const GLchar* shaderSource = shaderString.c_str();
+        glShaderSource( shaderId, 1, (const GLchar**)&shaderSource, NULL );
+        glCompileShader( shaderId );
+        GLint shaderCompiled = GL_FALSE;
+        glGetShaderiv( shaderId, GL_COMPILE_STATUS, &shaderCompiled );
+        if( shaderCompiled != GL_TRUE )
+        {
+            errorStd( "Can't compile shader. Id:" << shaderId << 
+              "Source:" << shaderSource );
+            printShaderLog( shaderId );
+            glDeleteShader( shaderId );
+            shaderId = 0;
+        }
     }
-    return texture;
+    else
+    {
+        errorStd( "Unable to open file:" << path.c_str() );
+    }
+    return shaderId;
 }
-
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // End namespace game.
 }
--- source/game/renderer.h
+++ source/game/renderer.h
@@ -5,23 +5,23 @@
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Local.
 #include "../command_line/base.h"
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// SDL and GL.
 #include <GL/glew.h>
 #include <GL/glu.h>
 #include <SDL_opengl.h>
-
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Forward decleration.
 class SDL_Window;
 class SDL_Renderer;
 class SDL_Texture;
-class SDL_Texture;
+class SDL_Surface;
 class SDL_Rect;
 class SDL_Color;
-
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 namespace game
 {
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 class Renderer
 {
 public:
@@ -33,64 +33,32 @@ protected:
 private:
     const commandLine::Base& m_cmdLine;
     SDL_Window* m_window;
-    SDL_Texture* m_background;
-    SDL_Texture* m_image;
+    SDL_Surface* m_surfaceLoul;
+    SDL_Surface* m_surfaceCat;
     SDL_GLContext m_context;
-    GLuint m_programIDbo = 0;
-    GLint m_vertexPos2DLocation = -1;
-    GLuint m_vbo = 0;
-    GLuint m_ibo = 0;
+    GLuint m_programId;
+    GLint m_vertexPos;
+    GLuint m_vbo;
+    GLuint m_ibo;
+
 
 // Functions
 public:
-    
-    bool init();
-
-    //Initializes rendering program and clear color
-    bool initGL();
-
-    //Input handler
-    void handleKeys( unsigned char key, int x, int y );
-    //Renders quad to the screen
-    void render();
-
-    //Frees media and shuts down SDL
-    void close();
-//Shader loading utility programs
-void printProgramLog( GLuint program );
-void printShaderLog( GLuint shader );
-
-
     Renderer( const commandLine::Base& arguments, SDL_Window* window );
     bool initalise();
+    bool initGL();
     bool shutDown();
     void update();
-    bool draw( SDL_Texture* texture
-             , int x
-             , int y
-             , double rotation );
-    bool draw( SDL_Texture* texture
-             , int x
-             , int y
-             , int x2
-             , int y2
-             , int w
-             , int h
-             , double rotation );
-    bool draw( SDL_Texture* texture
-             , int x
-             , int y
-             , SDL_Rect& rect
-             , double rotation );
-    bool draw( int x
-             , int y
-             , int w
-             , int h
-             , SDL_Color& color
-             , double rotation );
+    void render();
     SDL_Texture* loadTexture( const std::string &file );
+    SDL_Surface* loadBmpToSurface( const std::string &file );
+    void printProgramLog( GLuint program );
+    void printShaderLog( GLuint shader );
+    GLuint loadShaderFromFile( std::string path, GLenum shaderType );
 protected:
 private:
 };
-
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// End namespace game.
 }
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
